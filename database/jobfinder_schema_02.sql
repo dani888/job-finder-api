bb2ec22ece3f4e003c9635e96f81b47a8d2b3f1f
Daniel Werminghausen <daniel.werminghausen@gmail.com>
2021-05-04 21:00:28 -0400

    update

--- database/jobfinder_schema_02.sql
+++ database/jobfinder_schema_02.sql
@@ -1,38 +1,22 @@
 CREATE TABLE liked (
     ID UUID PRIMARY KEY,
-    refID UUID References job;
+    job_id UUID References job(ID)
 );
-/* "SELECT * FROM j.jobs l.liked
-WHERE j.ID = l.refID  and j.title NOT LIKE '%Senior%' 
-                    AND j.title NOT LIKE '%SENIOR%' 
-                    AND j.title NOT LIKE '%senior%' 
-                    AND j.title NOT LIKE '%Sr%'
-                    AND j.title NOT LIKE '%Lead%';" */
+
 
 
 CREATE TABLE applied (
     ID UUID PRIMARY KEY,
-    refID UUID References job;
+    job_id UUID References job(ID)
 );
 
-/* "SELECT * FROM j.jobs a.applied
-WHERE j.ID = l.refID and j.title NOT LIKE '%Senior%' 
-                    AND j.title NOT LIKE '%SENIOR%' 
-                    AND j.title NOT LIKE '%senior%' 
-                    AND j.title NOT LIKE '%Sr%'
-                    AND j.title NOT LIKE '%Lead%';" */
 
 CREATE TABLE passed (
     ID UUID PRIMARY KEY,
-    refID UUID References job;
+    job_id UUID References job(ID)
 );
 
-/* "SELECT * FROM j.jobs p.passed
-WHERE j.ID = l.refID" and j.title NOT LIKE '%Senior%' 
-                    AND j.title NOT LIKE '%SENIOR%' 
-                    AND j.title NOT LIKE '%senior%' 
-                    AND j.title NOT LIKE '%Sr%'
-                    AND j.title NOT LIKE '%Lead%';"*/
+
 
 
 
